f064466bc51b75515912da67340d21eb84338c69
Iarivosoa <iarivosoaf@gmail.com>
2025-05-14 08:10:46 +0200

    Corrige nom du projet Django dans manage.py et render.yaml

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-exemple d'un site e-commerce avec une validation de paiement stripe ainsi des commentaires des utilisateurs sur les produits
+exemple d'un site e-commerce avec une validation de paiement stripe ainsi des commentaires des utilisateurs sur les produi
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'site_pratique_colo.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'site-vente.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
